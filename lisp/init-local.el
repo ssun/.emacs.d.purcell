0feb72aed4d632d9ac66d52d2e1af377c08aeab9
Steven Sun <steven@Stevens-MacBook-Air.local>
2016-07-09 22:03:53 -0400

    Ignore elpa. Include more local settings.

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -1,2 +1,15 @@
+;;; Magit Settings
 (global-set-key (kbd "C-x g") 'magit-status)
+
+;;; Semantic Settings
+(semantic-mode 1)
+(global-semantic-stickyfunc-mode 1)
+
+;;; Enable Time
+(display-time-mode 1)
+
+;;; C/C++ BSD style indentation
+(setq c-default-style "bsd"
+      c-basic-offset 2)
+
 (provide 'init-local)
